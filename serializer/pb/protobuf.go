86722b2b0c6fb99ed6885e94fb17164769f2aec7
毕康 <bikang1@staff.weibo.com>
2018-06-26 19:41:16 +0800

    go

--- serializer/pb/protobuf.go
+++ serializer/pb/protobuf.go
@@ -9,10 +9,12 @@ var errWrongValueType = errors.New("protobuf: convert on wrong type value")
 
 type ProtobufSerializer struct{}
 
-func New()*ProtobufSerializer{
+//定义protobuf
+func New() *ProtobufSerializer {
 	return &ProtobufSerializer{}
 }
 
+//proto Encode
 func (s *ProtobufSerializer) Encode(v interface{}) ([]byte, error) {
 	pb, ok := v.(proto.Message)
 	if !ok {
@@ -21,6 +23,7 @@ func (s *ProtobufSerializer) Encode(v interface{}) ([]byte, error) {
 	return proto.Marshal(pb)
 }
 
+//proto decode
 func (s *ProtobufSerializer) Decode(data []byte, v interface{}) error {
 	pb, ok := v.(proto.Message)
 	if !ok {
